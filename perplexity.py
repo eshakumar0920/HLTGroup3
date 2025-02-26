c72521eb9f29c0d9a51e9ac35a1bd9ceddc0a4db
Jocelyn Liu <jocelynliu347@gmail.com>
2025-02-26 13:54:28 -0600

    Updated perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -1,7 +1,4 @@
-import nltk     # Natural Language Toolkit
-import numpy as np
 import math
-
 
 def split_line_into_ngrams(n, line, prev_n_tokens):
     ngrams = []
@@ -16,11 +13,25 @@ def split_line_into_ngrams(n, line, prev_n_tokens):
     
     return ngrams  
 
+def accumulate_perplexity_values(n, tokens, prev_n_tokens):
+    line_ngrams = split_line_into_ngrams(n, tokens, prev_n_tokens)
+    #print("ngrams for this line: \n%s\n" % line_ngrams)
+    num_tokens_in_line = len(tokens)
+    #print(f"num_tokens_in_line: ", num_tokens_in_line)
+
+    line_log_probability_sum = 0
+    for ngram in line_ngrams:
+        probability = 4  # ?? need to calculate for this ngram
+        line_log_probability_sum += math.log(probability)
+
+    #print("line_log_probability_sum: %f\n" % line_log_probability_sum)
+    return num_tokens_in_line, line_log_probability_sum
+
 def get_perplexity_for_validation_data(file_name, n):
-    # variables to track total N and total sum values
+    # N and log_probability_sum are variables to track the values needed to calculate the perplexity
     N = 0                       # total number of tokens in validation set
     log_probability_sum = 0     # total sum of each n-gram's probability in the validation set
-    prev_n_tokens = []
+    prev_n_tokens = []          # list of the last n-1 tokens from the previous line
 
     # tokenize the validation corpus, one line (review) at a time
     with open(file_name, "r") as val_set:
@@ -37,25 +48,7 @@ def get_perplexity_for_validation_data(file_name, n):
             #print("UPDATED N, log_probability_sum: %s, %s\n" % (N, log_probability_sum))
         
     # After all lines in corpus are processed, get the perplexity for the validation set
-    return compute_perplexity(N, log_probability_sum)
-
-def accumulate_perplexity_values(n, tokens, prev_n_tokens):
-    line_ngrams = split_line_into_ngrams(n, tokens, prev_n_tokens)
-    #print("ngrams for this line: \n%s\n" % line_ngrams)
-    num_tokens_in_line = len(tokens)
-    #print(f"num_tokens_in_line: ", num_tokens_in_line)
-
-    line_log_probability_sum = 0
-    for ngram in line_ngrams:
-        probability = 4  # ?? need to calculate for this ngram
-        line_log_probability_sum += math.log(probability)
-
-    #print("line_log_probability_sum: %f\n" % line_log_probability_sum)
-    return num_tokens_in_line, line_log_probability_sum
-
-def compute_perplexity(N, log_probability_sum):
     return math.exp(-log_probability_sum / N)
 
 if __name__ == "__main__":
-    # Given DATASET contains "val.txt"  <-- validation set corpus!
     print("Perplexity: %.5f" % get_perplexity_for_validation_data("val.txt", 3))
